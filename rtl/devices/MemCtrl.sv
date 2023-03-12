fe1bb28ee1ce66e906a466eebfad957ee98a18fc
rishyak <hello@rishyak.com>
2023-03-12 00:42:03 -0500

    add: common interface
    Signed-off-by: rishyak <hello@rishyak.com>

---
+++ rtl/devices/MemCtrl.sv
@@ -0,0 +1,11 @@
+/**
+  @brief Memory Controller module that does memory controller controlling
+
+  @note Interface is on nyu-ambam, which will be included as a package
+*/
+module MemCtrl(
+  MemCommon_if.memCtrl mem 
+);
+  // Add memory controller logic here
+
+endmodule
