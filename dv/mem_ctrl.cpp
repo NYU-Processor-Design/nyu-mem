7fd577b30edd5d63c760b676a451f4bf20b228c2
Vito Gamberini <vito@gamberini.email>
2023-03-23 12:47:39 -0400

    fix: Write top-level wrapper for MemCtrl test

--- dv/mem_ctrl.cpp
+++ dv/mem_ctrl.cpp
@@ -1,8 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
 #include <cstdint>
 #include <iostream>
-#include <catch2/catch_test_macros.hpp>
 
-#include <VMemCtrl.h>
+#include <VMemCtrl_tl.h>
 
 
 using namespace std;
@@ -10,4 +10,3 @@ using namespace std;
 TEST_CASE("First Test") {
   REQUIRE(true);
 }
-
